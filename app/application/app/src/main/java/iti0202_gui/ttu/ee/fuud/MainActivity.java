a76c7a2b8e287fbac97afd547c815910fc1a723a
Janek <javalg@ttu.ee>
2018-04-03 10:25:55 +0300

    initial environment and testfiles

---
+++ app/application/app/src/main/java/iti0202_gui/ttu/ee/fuud/MainActivity.java
@@ -0,0 +1,19 @@
+package iti0202_gui.ttu.ee.fuud;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+    public static final String EXTRA_MESSAGE = "iti0202_gui.ttu.ee.fuud.MESSAGE";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void sendMessage(View view) {
+        
+    }
+}
