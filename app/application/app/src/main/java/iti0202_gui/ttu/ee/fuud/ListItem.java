f42b3c98de548435692d249ccd880f06610bcd81
Janek <javalg@ttu.ee>
2018-04-13 19:12:37 +0300

    Added RecyclerView with dummy text (Header and description)

---
+++ app/application/app/src/main/java/iti0202_gui/ttu/ee/fuud/ListItem.java
@@ -0,0 +1,20 @@
+package iti0202_gui.ttu.ee.fuud;
+
+public class ListItem {
+
+    private String head;
+    private String desc;
+
+    public ListItem(String head, String desc) {
+        this.head = head;
+        this.desc = desc;
+    }
+
+    public String getHead() {
+        return head;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+}
--- app/application/app/src/main/java/iti0202_gui/ttu/ee/fuud/MainActivity.java
+++ app/application/app/src/main/java/iti0202_gui/ttu/ee/fuud/MainActivity.java
@@ -2,18 +2,43 @@ package iti0202_gui.ttu.ee.fuud;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class MainActivity extends AppCompatActivity {
-    public static final String EXTRA_MESSAGE = "iti0202_gui.ttu.ee.fuud.MESSAGE";
+
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter adapter;
+
+    private List<ListItem> listItems;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        recyclerView.setHasFixedSize(true); // Each card item is the same size
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        listItems = new ArrayList<>();
+
+        // Where items go to the list
+        for (int i=0; i <= 10; i++) {
+            ListItem listItem = new ListItem(
+                    "heading " + (i+1),
+                    "description stuff here"
+            );
+            listItems.add(listItem);
+        }
+
+        adapter = new MyAdapter(listItems, this); // Makes an instance of the adapter with the list of items above
+        recyclerView.setAdapter(adapter);
     }
 
-    public void sendMessage(View view) {
-        
-    }
+
 }
