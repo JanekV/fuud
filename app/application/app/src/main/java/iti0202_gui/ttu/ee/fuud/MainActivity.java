7d77449290b232e9b610fbdc124637bdef1fc3ae
Janek <javalg@ttu.ee>
2018-04-13 20:51:05 +0300

    Added basis for reading JSON from the web.

--- app/application/app/src/main/java/iti0202_gui/ttu/ee/fuud/MainActivity.java
+++ app/application/app/src/main/java/iti0202_gui/ttu/ee/fuud/MainActivity.java
@@ -1,9 +1,22 @@
 package iti0202_gui.ttu.ee.fuud;
 
+import android.app.ProgressDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,6 +24,7 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final String BITSTOP_DATA = "192.162..:8080/bitstop";
     private RecyclerView recyclerView;
     private RecyclerView.Adapter adapter;
 
@@ -28,16 +42,57 @@ public class MainActivity extends AppCompatActivity {
         listItems = new ArrayList<>();
 
         // Where items go to the list
-        for (int i=0; i <= 10; i++) {
-            ListItem listItem = new ListItem(
-                    "heading " + (i+1),
-                    "description stuff here"
-            );
-            listItems.add(listItem);
-        }
+        loadRecyclerViewData();
 
-        adapter = new MyAdapter(listItems, this); // Makes an instance of the adapter with the list of items above
-        recyclerView.setAdapter(adapter);
+
+
+    }
+
+    private void loadRecyclerViewData() {
+        final ProgressDialog progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage("Loadning data...");
+        progressDialog.show();
+
+        StringRequest stringRequest = new StringRequest(Request.Method.GET,
+                BITSTOP_DATA,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String s) {
+                        progressDialog.dismiss();
+                        try {
+                            JSONObject jsonObject = new JSONObject(s);
+                            JSONArray array = jsonObject.getJSONArray("data");
+
+                            for (int i = 0; i < array.length(); i++) { // for each item (meal)
+                                JSONObject o = array.getJSONObject(i);
+                                ListItem item = new ListItem(           // get data about the meal
+                                        o.getString("provider"),
+                                        o.getString("name_eng")
+                                        /*
+                                        o.getString("name_est"),
+                                        o.getString("price"),
+                                        o.getString("name-eng")
+                                        */
+                                );
+                            }
+
+                            adapter = new MyAdapter(listItems, getApplicationContext()); // Makes an instance of the adapter with the list of items above
+                            recyclerView.setAdapter(adapter);
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        progressDialog.dismiss();
+                        Toast.makeText(getApplicationContext(), error.getMessage(), Toast.LENGTH_LONG).show();
+                    }
+                });
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
     }
 
 
