613e353dcb178c4e5de8d75a8cfaff6cf9a42ac5
robert.mannik <mannik.robert@gmail.com>
2018-04-17 17:12:21 +0300

    Daily pdf parser 1.1 - refactor 1.

--- server/parsers/src/daily/src/main/java/ee/ttu/dailyparser/DailyMain.java
+++ server/parsers/src/daily/src/main/java/ee/ttu/dailyparser/DailyMain.java
@@ -2,42 +2,62 @@ package ee.ttu.dailyparser;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.*;
 
 public class DailyMain {
-    public static void main(String[] args) throws IOException {
-        final String keemiaLink = "http://www.daily.ee/files/dn_daily_nadalamenuu_ttu_keemiainstituut.pdf";
-        final String itMajaLink = "http://www.daily.ee/files/dn_daily_nadalamenuu_ttu_it_maja.pdf";
-        final String peamajaLink = "http://www.daily.ee/files/dn_daily_nadalamenuu_ttu_peahoone.pdf";
-        final String neljasKorpusLink = "http://www.daily.ee/files/dn_daily_nadalamenuu_ttu_4_korpus.pdf";
+    private static final String KEEMIA_LINK = "http://www.daily.ee/files/dn_daily_nadalamenuu_ttu_keemiainstituut.pdf";
+    private static final String IT_MAJA_LINK = "http://www.daily.ee/files/dn_daily_nadalamenuu_ttu_it_maja.pdf";
+    private static final String PEAMAJA_LINK = "http://www.daily.ee/files/dn_daily_nadalamenuu_ttu_peahoone.pdf";
+    private static final String NELJAS_KORPUS_LINK = "http://www.daily.ee/files/dn_daily_nadalamenuu_ttu_4_korpus.pdf";
 
-        new DailyDownloader(keemiaLink, "keemia").download();
-        new DailyDownloader(itMajaLink, "it_maja").download();
-        new DailyDownloader(peamajaLink, "peamaja").download();
-        new DailyDownloader(neljasKorpusLink, "neljas_korpus").download();
+    private static final String IT_MAJA_PDF = "C:/file/it_maja.pdf";
+    private static final String KEEMIA_PDF = "C:/file/keemia.pdf";
+    private static final String PEAMAJA_PDF = "C:/file/peamaja.pdf";
+    private static final String NELJAS_KORPUS_PDF = "C:/file/neljas_korpus.pdf";
 
-        final String itMajaPdf = "C:/file/it_maja.pdf";
-        final String keemiaPdf = "C:/file/keemia.pdf";
-        final String peamajaPdf = "C:/file/peamaja.pdf";
-        final String neljasKorpusPdf = "C:/file/neljas_korpus.pdf";
+    private static final String IT_MAJA = "it_maja";
+    private static final String KEEMIA = "keemia";
+    private static final String PEAMAJA = "peamaja";
+    private static final String NELJAS_KORPUS = "neljas_korpus";
 
-        DailyReader itMajaReader = new DailyReader(new File(itMajaPdf));
-        DailyReader keemiaReader = new DailyReader(new File(keemiaPdf));
-        DailyReader peamajaReader = new DailyReader(new File(peamajaPdf));
-        DailyReader neljasKorpusReader = new DailyReader(new File(neljasKorpusPdf));
+    public DailyMain() {
+
+    }
+
+    public String downloadMenus() throws IOException {
+        if (new DailyDownloader(KEEMIA_LINK, "keemia").download()
+                && new DailyDownloader(IT_MAJA_LINK, "it_maja").download()
+                && new DailyDownloader(PEAMAJA_LINK, "peamaja").download()
+                && new DailyDownloader(NELJAS_KORPUS_LINK, "neljas_korpus").download()) {
+            return "Files downloaded!";
+        } else {
+            return "Error, please investigate!";
+        }
+    }
+
+    public List<Map<String, String>> getMenuData() throws IOException {
+        List<Map<String, String>> menuData = new LinkedList<>();
+
+        DailyReader itMajaReader = new DailyReader(new File(IT_MAJA_PDF));
+        DailyReader keemiaReader = new DailyReader(new File(KEEMIA_PDF));
+        DailyReader peamajaReader = new DailyReader(new File(PEAMAJA_PDF));
+        DailyReader neljasKorpusReader = new DailyReader(new File(NELJAS_KORPUS_PDF));
 
         DailyMenuSplitter itMajaSplitter = new DailyMenuSplitter(itMajaReader.getMenuAsLinkedList());
         DailyMenuSplitter keemiaSplitter = new DailyMenuSplitter(keemiaReader.getMenuAsLinkedList());
         DailyMenuSplitter peamajaSplitter = new DailyMenuSplitter(peamajaReader.getMenuAsLinkedList());
         DailyMenuSplitter neljasKorpusSplitter = new DailyMenuSplitter(neljasKorpusReader.getMenuAsLinkedList());
 
-        DailyParser itMajaParser = new DailyParser(itMajaSplitter.getByToday());
-        DailyParser keemiaParser = new DailyParser(keemiaSplitter.getByToday());
-        DailyParser peamajaParser = new DailyParser(peamajaSplitter.getByToday());
-        DailyParser neljasKorpusParser = new DailyParser(neljasKorpusSplitter.getByToday());
+        DailyParser itMajaParser = new DailyParser(itMajaSplitter.getByToday(), IT_MAJA);
+        DailyParser keemiaParser = new DailyParser(keemiaSplitter.getByToday(), KEEMIA);
+        DailyParser peamajaParser = new DailyParser(peamajaSplitter.getByToday(), PEAMAJA);
+        DailyParser neljasKorpusParser = new DailyParser(neljasKorpusSplitter.getByToday(), NELJAS_KORPUS);
 
-        System.out.println(itMajaParser.getFinalMenu());
-        System.out.println(keemiaParser.getFinalMenu());
-        System.out.println(peamajaParser.getFinalMenu());
-        System.out.println(neljasKorpusParser.getFinalMenu());
+        menuData.addAll(itMajaParser.getFinalMenu());
+        menuData.addAll(keemiaParser.getFinalMenu());
+        menuData.addAll(peamajaParser.getFinalMenu());
+        menuData.addAll(neljasKorpusParser.getFinalMenu());
+
+        return menuData;
     }
 }
--- server/parsers/src/daily/src/main/java/ee/ttu/dailyparser/DailyParser.java
+++ server/parsers/src/daily/src/main/java/ee/ttu/dailyparser/DailyParser.java
@@ -2,30 +2,32 @@ package ee.ttu.dailyparser;
 
 import java.util.HashMap;
 import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
 
 public class DailyParser {
     enum status {
         OPEN, CLOSED
     }
-    private LinkedList<HashMap<String, String>> finalMenu;
-    private LinkedList<String> menuToBeParsed;
-    private HashMap<String, String> providerStatus;
+    private List<Map<String, String>> finalMenu;
+    private List<String> menuToBeParsed;
+    private Map<String, String> providerStatus;
 
     /**
      * Constructor for parsing Daily menu in a linked list form.
      * @param splitMenu incoming menu as a linked list.
      */
-    DailyParser(LinkedList<String> splitMenu) {
+    DailyParser(List<String> splitMenu, String facility) {
         this.finalMenu = new LinkedList<>();
         this.menuToBeParsed = splitMenu;
-        parseMenu();
+        parseMenu(facility);
     }
 
     /**
      * Getter method for receiving the menu as a linked list of hashmaps.
      * @return menu as a linked list of hashmaps.
      */
-    LinkedList<HashMap<String, String>> getFinalMenu() {
+    List<Map<String, String>> getFinalMenu() {
         return finalMenu;
     }
 
@@ -40,21 +42,21 @@ public class DailyParser {
      * name_eng - name of the dish in English.
      * price - price of the dish.
      */
-    private void parseMenu() {
+    private void parseMenu(String facility) {
         // Sets establishment status to closed if the menu size is less than 2.
         if (menuToBeParsed.size() < 2) {
-            setStatus(status.CLOSED);
+            setStatus(status.CLOSED, facility);
             finalMenu.add(providerStatus);
         } else {
             // Converts the menu into a linked list of hashmaps and sets status to open.
-            setStatus(status.OPEN);
+            setStatus(status.OPEN, facility);
             finalMenu.add(providerStatus);
             //For loop converts every menu item into a hashmap, inserts them into a linked list.
             for (int i = 0; i < menuToBeParsed.size(); i += 2) {
-                HashMap<String, String> menuItem = new HashMap<>();
+                Map<String, String> menuItem = new HashMap<>();
                 String nameWithPrice = menuToBeParsed.get(i);
                 Integer priceBeginIndex = nameWithPrice.length() - 4;
-                menuItem.put("provider", "daily");
+                menuItem.put("provider", "daily_" + facility);
                 menuItem.put("name_est", nameWithPrice.substring(0, priceBeginIndex).trim());
                 menuItem.put("price", nameWithPrice.substring(priceBeginIndex));
                 menuItem.put("name_eng", menuToBeParsed.get(i + 1).trim());
@@ -67,9 +69,9 @@ public class DailyParser {
      * Method for setting the open or closed status for the establishment.
      * @param status open or closed as enum.
      */
-    private void setStatus(Enum status) {
-        HashMap<String, String> providerStatus = new HashMap<>();
-        providerStatus.put("provider", "daily");
+    private void setStatus(Enum status, String facility) {
+        Map<String, String> providerStatus = new HashMap<>();
+        providerStatus.put("provider", "daily_" + facility);
         providerStatus.put("status", status.toString());
         this.providerStatus = providerStatus;
     }
